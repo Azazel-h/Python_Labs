eccccec1080543618e0bb1155647800f22629717
Azazel <yasuhiro.corp@gmail.com>
2022-02-20 21:22:47 +0300

    First GUI python3 lab completed

--- Python_second_sem/Lab_1/widget.py
+++ Python_second_sem/Lab_1/widget.py
@@ -1,17 +1,17 @@
 import sys
-from PyQt6 import QtCore, QtGui, QtWidgets
+import math
+from PyQt6 import QtWidgets
+from PyQt6.QtWidgets import QDialog
+from PyQt6.uic.properties import QtCore
+
 from form import Ui_MainWindow
+from about import Ui_Dialog
 
 
 class Calculator(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, obj=None, **kwargs):
         super(Calculator, self).__init__(*args, **kwargs)
         self.setupUi(self)
-        '''pattern = '(0-9)'
-        re = QtCore.QRegularExpression(pattern)
-        validator = QtGui.QRegularExpressionValidator(re, self)
-        print(validator.regularExpression())
-        self.lineEdit.setValidator(validator)'''
         self.lineEdit.setText('0')
         self.pushButton.clicked.connect(self.digit_pressed)  # 1
         self.pushButton_2.clicked.connect(self.digit_pressed)  # 2
@@ -23,75 +23,102 @@ class Calculator(QtWidgets.QMainWindow, Ui_MainWindow):
         self.pushButton_8.clicked.connect(self.digit_pressed)  # 8
         self.pushButton_9.clicked.connect(self.digit_pressed)  # 9
         self.pushButton_10.clicked.connect(self.digit_pressed)  # 0
+        self.pushButton_14.clicked.connect(self.digit_pressed)
+        self.pushButton_15.clicked.connect(self.digit_pressed)
         self.pushButton_11.clicked.connect(self.quinary_pressed)
         self.pushButton_12.clicked.connect(self.declaminal_pressed)
         self.pushButton_13.clicked.connect(self.clear)
-        self.pushButton_14.clicked.connect(self.digit_pressed)
+        self.actionClear_Input.triggered.connect(self.clear)
+        self.actionTo_5th_from_10th.triggered.connect(self.quinary_pressed)
+        self.actionTo_10th_from_5th.triggered.connect(self.declaminal_pressed)
+        self.actionAbout_the_creator_2.triggered.connect(self.about)
         self.show()
 
+    def about(self):
+        dialog = QDialog()
+        dialog.ui = Ui_Dialog()
+        dialog.ui.setupUi(dialog)
+        dialog.exec()
+
+    def float_num_validation(self, input):
+        is_float = False
+        try:
+            input = float(input)
+            is_float = True
+        finally:
+            return is_float
+
+
     def quinary_pressed(self):
-        entered_num = str(float(self.lineEdit.text()))
-        result = self.quinary_convertion(entered_num)
+        entered_num = self.lineEdit.text()
+        if not self.float_num_validation(entered_num):
+            self.label.setText("ERROR: Isn't a number!")
+            return -1
+        result = self.conversion(float(entered_num), 5, int(self.spinBox.text()))
         self.lineEdit.setText(result)
 
+
     def declaminal_pressed(self):
-        entered_num = str(float(self.lineEdit.text()))
+        entered_num = self.lineEdit.text()
+        if not self.float_num_validation(entered_num):
+            self.label.setText("ERROR: Isn't a number!")
+            return -1
         if [i for i in '56789' if i in entered_num]:
             self.label.setText("ERROR: Isn't a quinary number!")
             return -1
-        self.label.setText("")
-        result = self.declaminal_convertion(entered_num)
+        self.label.setText('')
+        result = self.conversion(str(float(entered_num)), 5, int(self.spinBox.text()))
         self.lineEdit.setText(result)
 
     def clear(self):
         self.lineEdit.setText('0')
 
-    def quinary_convertion(self, num):
-        num = str(float(num)).split('.')
-        new = ''
-        sign = ''
-        if num[0][0] == '-' or num[0][0] == '+':
-            sign = num[0][0]
-            num[0] = num[0][1:]
 
-        num[0] = int(num[0])
-        while num[0]:
-            new += str(num[0] % 5)
-            num[0] //= 5
+    def conversion(self, num, to_radix, maxlen):
+        def i_conversion(n, radix=5):
+            new = []
+            while n:
+                n, p = divmod(n, radix)
+                new.append('%d' % p)
+                if maxlen and len(new) > maxlen:
+                    break
+            new.reverse()
+            return ''.join(new)
 
-        new = new[::-1]
-        new += '.'
+        def f_conversion(n, radix=5):
+            new = []
+            f = math.modf(n)[0]
+            while f:
+                f, p = math.modf(f * radix)
+                new.append('%.0f' % p)
+                if maxlen and len(new) > maxlen:
+                    break
+            return ''.join(new)
 
-        num[1] = float('0.' + num[1])
-        for i in range(int(self.spinBox.text())):
-            num[1] = str(float(num[1]) * 5)
-            new += num[1][0]
-            num[1] = '0' + num[1][1:]
+        def extract_sign(num):
+            num = str(num)
+            sign = ''
+            if num[0] == '-':
+                sign = '-'
+                num = num[1:]
+            return sign, float(num)
 
-        print(float(sign + str(new)))
-        return f'{float(sign + str(new)):.{int(self.spinBox.text())}f}'
+        if isinstance(num, float):
+            sign, num = extract_sign(num)
+
+            return sign + (i_conversion(num, to_radix) if i_conversion(num, to_radix) else '0') + '.'\
+                   + (f_conversion(num, to_radix) if f_conversion(num, to_radix) else '0')
+
+        elif isinstance(num, str):
+            sign, num = extract_sign(num)
+
+            f_string = '{:.' + str(maxlen) + 'f}'
+            n, f = f_string.format(num).split('.')
+            n = int(n, to_radix)
+            f = int(f, to_radix) / float(to_radix ** len(f))
+            return sign + str(float(n + f))
 
 
-    def declaminal_convertion(self, num):
-        num = str(num).split('.')
-        new = 0
-
-        sign = ''
-        if num[0][0] == '-' or num[0][0] == '+':
-            sign = num[0][0]
-            num[0] = num[0][1:]
-
-        counter = 0
-        for i in num[0][::-1]:
-            new += float(i) * (5 ** counter)
-            counter += 1
-
-        counter = 1
-        for j in num[1]:
-            new += float(j) * (5 ** -counter)
-            counter += 1
-        print(float(sign + str(new)))
-        return str(float(sign + str(new)))
 
     def digit_pressed(self):
         button = self.sender()
@@ -101,7 +128,7 @@ class Calculator(QtWidgets.QMainWindow, Ui_MainWindow):
         else:
             self.lineEdit.setText(self.lineEdit.text() + button.text())
 
-
-app = QtWidgets.QApplication(sys.argv)
-window = Calculator()
-app.exec()
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = Calculator()
+    app.exec()
